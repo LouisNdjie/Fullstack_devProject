48dbc4b91acea3a3892f5513982af494088ce0d1
BEGINSAN <fernandalouka345@gmail.com>
2025-02-11 16:23:34 +0100

    update

---
+++ QUIZZ_APP/JS/main.js
@@ -0,0 +1,18 @@
+function openform() {
+    document.getElementById('hide').style.display = 'block';
+}
+function closeform() {
+    document.getElementById('hide').style.display = 'none';
+}
+function forminscript() {
+    document.getElementById('inscription').style.display = 'block';
+}
+function closeinscript() {
+    document.getElementById('inscription').style.display = 'none';
+}
+function formcon() {
+    document.getElementById('connexion').style.display = 'block';
+}
+function closecon() {
+    document.getElementById('connexion').style.display = 'none';
+} 
